edca60bde99383ef7aa2bc701c913639412e92af
JuanBonillla <juanbonilla2116@gmail.com>
2024-01-01 21:58:46 -0500

    parte 4.3

--- bin/http/ControladorUsuarios.php
+++ bin/http/ControladorUsuarios.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 class ControladorUsuarios {
 
     function __construct() {
@@ -11,26 +9,24 @@ class ControladorUsuarios {
     public function insertarUsuario($usuario) {
         $usuarioModel = new Usuarios();
         $id = $usuarioModel->insert($usuario);
-        return [
-            "codigo" => (($id > 0) ? 1 : -1),
-            "mensaje" => ($id > 0) ? "Se ha insertado el usuario correctamente" : "No se pudo insertar el usuario.",
-            "datos" => $id
-        ];
+        $insersionExitosa = ($id > 0);
+        $respuesta = new Respuesta($insersionExitosa ? EMensajes::INSERCION_EXITOSA : EMensajes::ERROR_INSERSION);
+        $respuesta->setDatos($id);
+        return $respuesta;
     }
 
     public function listarUsuarios() {
         $usuarioModel = new Usuarios();
         $lista = $usuarioModel->get();
-        return [
-            "codigo" => ((count($lista) > 0) ? 1 : -1),
-            "mensaje" => ((count($lista) > 0) ? "Se han consultado los registros correctamente." : "No hay registros."),
-            "datos" => $lista
-        ];
+        $v = count($lista);
+        $respuesta = new Respuesta($v ? EMensajes::CORRECTO : EMensajes::ERROR);
+        $respuesta->setDatos($lista);
+        return $respuesta;
     }
 
     public function actualizarUsuario($usuario) {
         $usuarioModel = new Usuarios();
-        $actualizados = $usuarioModel->where("id", '=', $usuario["idUsuario"])
+        $actualizados = $usuarioModel->where("id", " =>", $usuario["idUsuario"])
                 ->update($usuario);
         return [
             "codigo" => (($actualizados > 0) ? 1 : -1),
@@ -41,7 +37,7 @@ class ControladorUsuarios {
 
     public function eliminarUsuario($idUsaurio) {
         $usuarioModel = new Usuarios();
-        $eliminados = $usuarioModel->where("id", '=', $idUsaurio)->delete();
+        $eliminados = $usuarioModel->where("id", " =>", $idUsaurio)->delete();
         return [
             "codigo" => (($eliminados > 0) ? 1 : -1),
             "mensaje" => ($eliminados > 0) ? "Se ha eliminado el usuario correctamente." : "No se pudo eliminado el usuario.",
@@ -51,7 +47,7 @@ class ControladorUsuarios {
 
     public function buscarUsuarioPorId($idUsuario) {
         $usuarioModel = new Usuarios();
-        $usuario = $usuarioModel->where("id", '=', $idUsuario)->first();
+        $usuario = $usuarioModel->where("id", " =>", $idUsuario)->first();
         return [
             "codigo" => (($usuario != null) ? 1 : -1),
             "mensaje" => ($usuario != null) ? "Se ha consultado el usuario correctamente." : "No se pudo consultar el usuario.",
--- index.php
+++ index.php
@@ -5,35 +5,31 @@ require_once './bin/persistencia/Crud.php';
 require_once './bin/persistencia/modelos/ModeloGenerico.php';
 require_once './bin/persistencia/modelos/Usuarios.php';
 require_once './bin/http/ControladorUsuarios.php';
-
-
+require_once './bin/constantes/EMensajes.php';
+require_once './bin/http/Respuesta.php';
 
 $controladorUsuarios = new ControladorUsuarios();
-
-/*$respuesta = $controladorUsuarios->insertarUsuario([
-    "nombres" => "JJ2",
-    "edad" => 22,
-    "email" => "email3@gmail.com",
-    "asdfasfda" => "sdfasdfa"
-]);/*
-/*$usuario = [
-    "idUsuario" => 6,
-    "correo" => "correo@gmail.com",
-    "telefono" => "123456789"
-];
-$respuesta = $controladorUsuarios->actualizarUsuario($usuario);
-var_dump($respuesta);
-echo "<br/>";
-*/
+//$respuesta = $controladorUsuarios->insertarUsuario([
+//    "nombres" => "JJ2",
+//    "edad" => 22,
+//    "email" => "email3@gmail.com",
+//    "asdfasfda" => "sdfasdfa"
+//]);
+//$usuario = [
+//    "idUsuario" => 6,
+//    "correo" => "correo@gmail.com",
+//    "telefono" => "123456789"
+//];
+//$respuesta = $controladorUsuarios->actualizarUsuario($usuario);
+//var_dump($respuesta);
+//echo "<br/>";
 //
 //$respuesta = $controladorUsuarios->eliminarUsuario(6);
 //var_dump($respuesta);
 //echo "<br/>";
-
-$respuesta = $controladorUsuarios->buscarUsuarioPorId(5);
-var_dump($respuesta);
-echo "<br/>";
-
-//echo "<br/>";
-//$respuesta = $controladorUsuarios->listarUsuarios();
+//$respuesta = $controladorUsuarios->buscarUsuarioPorId(6);
 //var_dump($respuesta);
+//echo "<br/>";
+
+$respuesta = $controladorUsuarios->listarUsuarios()->json();
+echo ($respuesta);
